a90ffe2a629bd483bbd5b4f4a5ea7374b3a69248
Jyoti Sharma <jyoti.sharma@sakec.ac.in>
2022-04-26 18:27:38 +0530

    fist commit

---
+++ javaproject/src/javaproject/fv.java
@@ -0,0 +1,23 @@
+package javaproject;
+
+final class bike
+{
+	final void run()
+	{
+		System.out.println("Welcome");
+	}
+}
+class fv extends bike{
+	void run() 
+	{
+		
+	}
+}
+public class fv {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Welcome");
+	}
+
+}
